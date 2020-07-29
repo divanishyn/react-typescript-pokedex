4ea0acd54bf1804b686e31c3649393560a6939c6
Dima Ivanishyn <dima.zt@gmail.com>
2020-07-29 21:27:09 +0300

    Remove default markup/css/logo.

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,14 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <main className="app">
+      <header className="header">
+        <img src="https://upload.wikimedia.org/wikipedia/commons/9/98/International_Pok%C3%A9mon_logo.svg" alt="logo" />
+        <h1>Pokedex</h1>
       </header>
-    </div>
+    </main>
   );
 }
 

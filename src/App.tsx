3d56a0c7b54b5b87e2188a4188a8601a426d16df
Dima Ivanishyn <dima.zt@gmail.com>
2020-07-29 22:01:17 +0300

    Add basic layout for landing page (album).

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import PokemonList from './components/PokemonList';
 
 function App() {
   return (
-    <main className="app">
-      <header className="header">
-        <img src="https://upload.wikimedia.org/wikipedia/commons/9/98/International_Pok%C3%A9mon_logo.svg" alt="logo" />
-        <h1>Pokedex</h1>
-      </header>
-    </main>
+    <>
+      <CssBaseline />
+      <div className="app">
+        <PokemonList />
+      </div>
+    </>
   );
 }
 

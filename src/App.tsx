87f95f016871ac46a6901e89f22eea206035e53b
Dima Ivanishyn <dima.zt@gmail.com>
2020-08-05 21:28:13 +0300

    Add routes

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,19 @@ import React from 'react';
 import './App.css';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import PokemonList from './components/PokemonList';
+import PokemonDetails from './components/PokemonDetails';
+
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 
 function App() {
   return (
     <>
       <CssBaseline />
       <div className="app">
-        <PokemonList />
+        <Router>
+          <Route path="/" exact component={PokemonList} />
+          <Route path="/details/:name" component={PokemonDetails} />
+        </Router>
       </div>
     </>
   );
